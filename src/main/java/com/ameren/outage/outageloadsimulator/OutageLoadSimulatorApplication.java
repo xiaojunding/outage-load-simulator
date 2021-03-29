a8e1b70196061201f1c54560424c8864c9b82653
xiaojunding <E149473@ameren.com>
2021-03-29 16:10:34 -0500

    skeleton

---
+++ src/main/java/com/ameren/outage/outageloadsimulator/OutageLoadSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.ameren.outage.outageloadsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutageLoadSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutageLoadSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ameren/outage/outageloadsimulator/OutageLoadSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ameren.outage.outageloadsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutageLoadSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
